4caaea4d929530da54a34e3369093d2b2124c297
Fabyan Yastika Permana <fabyanxrpl@gmail.com>
2023-11-30 11:58:19 +0700

    add register page

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,116 @@
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 export default function Register() {
+  const { push } = useRouter();
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    setError("");
+    setIsLoading(true);
+
+    // Touch Ke API
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        fullname: e.target.fullname.value,
+        email: e.target.email.value,
+        password: e.target.password.value,
+      }),
+    });
+    if (res.status === 200) {
+      e.target.reset();
+      push("/login");
+      setIsLoading(false);
+    } else {
+      setError("Email Already Exist");
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <>
+      <div className="flex justify-center items-center ">
+        <div className="flex w-full">
+          <div className="w-1/2 bg-slate-100 min-h-screen">
+            <div className="flex flex-col items-center justify-center h-screen">
+              {error !== "" && (
+                <h1 className="text-red-600 font-bold mb-5">{error}</h1>
+              )}
+              <div className="bg-white/50 border-2 border-slate-200 backdrop-blur-md w-fit h-fit rounded-xl p-4">
+                <h1 className="text-center">Login</h1>
+                <div className="flex justify-center">
+                  <form
+                    className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
+                    method="post"
+                    onSubmit={(e) => handleSubmit(e)}
+                  >
+                    <div className="mb-4 flex flex-col gap-6">
+                      <div className="relative h-11 w-full min-w-[200px]">
+                        <input
+                          name="fullname"
+                          type="text"
+                          className="peer h-full w-full rounded-md border border-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                        />
+                        <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                          FullName
+                        </label>
+                      </div>
+                      <div className="relative h-11 w-full min-w-[200px]">
+                        <input
+                          type="email"
+                          name="email"
+                          className="peer h-full w-full rounded-md border border-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                        />
+                        <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                          Email
+                        </label>
+                      </div>
+                      <div className="relative h-11 w-full min-w-[200px]">
+                        <input
+                          type="password"
+                          name="password"
+                          className="peer h-full w-full rounded-md border border-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                        />
+                        <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                          Password
+                        </label>
+                      </div>
+                    </div>
+
+                    <button
+                      className="mt-6 block w-full select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                      type="submit"
+                      data-ripple-light="true"
+                    >
+                      Register
+                    </button>
+                    <p className="mt-4 block text-center font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
+                      Already Have an Account?
+                      <Link
+                        className="font-semibold text-blue-500 transition-colors hover:text-blue-700"
+                        href="/login"
+                      >
+                        Sign In
+                      </Link>
+                    </p>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-1/2 bg-blue-500 min-h-screen"></div>
+        </div>
+        {/* <div className="bg-white shadow-xl w-96 h-40 rounded-xl p-4 border-2 border-slate/30">
+        <h1 className="text-center">Login</h1>
+      </div> */}
+      </div>
+    </>
   );
 }
--- src/lib/firebase/service.ts
+++ src/lib/firebase/service.ts
@@ -1,9 +1,20 @@
 // AMBIL DATA
-import { getDocs, getFirestore, collection,getDoc,  doc} from "firebase/firestore";
+import {
+  getDocs,
+  getFirestore,
+  collection,
+  getDoc,
+  doc,
+  query,
+  where,
+  addDoc,
+} from "firebase/firestore";
 import app from "./init";
 
+import bcrypt from "bcrypt";
 const firestore = getFirestore(app);
 
+// Terima Data
 export async function retrieveData(collectionName: string) {
   const snapshot = await getDocs(collection(firestore, collectionName));
   const data = snapshot.docs.map((doc) => ({
@@ -13,9 +24,43 @@ export async function retrieveData(collectionName: string) {
 
   return data;
 }
-
+// Terima Data per ID
 export async function retriveDataById(collectionName: string, id: string) {
   const snapshot = await getDoc(doc(firestore, collectionName, id));
   const data = snapshot.data();
   return data;
+}
+
+// Register
+export async function register(data: {
+  fullname: string;
+  email: string;
+  password: string;
+  role?: string; //Opsional menggunakan ?
+}) {
+  const q = query(
+    collection(firestore, "users"),
+    where("email", "==", data.email)
+  );
+  const snapshot = await getDocs(q);
+  const users = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  if (users.length > 0) {
+    return { status: false, statusCode: 400, message: "Email Still Exist!" };
+  } else {
+    data.role = "admin";
+    data.password = await bcrypt.hash(data.password, 10);
+
+    try {
+      // Upload ke FireStore
+      await addDoc(collection(firestore, "users"), data);
+
+      return { status: true, statusCode: 200, message: "Berhasil Register" };
+    } catch (error) {
+      return { status: false, statusCode: 400, message: "Register Gagal" };
+    }
+  }
 }
